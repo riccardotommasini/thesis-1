863737e723db6f92dd1fdd45a20109f33ee41149
Philippe scorsolini <pscorso93@gmail.com>
2018-09-22 10:44:50 +0200

    initial boilerplate

---
+++ Metamorphosys/src/main/java/phisco/streams/polimi/it/App.java
@@ -0,0 +1,13 @@
+package phisco.streams.polimi.it;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
