9f6680bed825fa3d6a578c9cad263dc332ce3515
Philippe scorsolini <pscorso93@gmail.com>
2018-10-07 12:24:39 +0200

    added scripts to ksql-cli container and added script

---
+++ scripts/ksql.sql
@@ -0,0 +1,21 @@
+create stream streaming_data (
+	DATE     BIGINT,
+	DEV_SN   VARCHAR(STRING),
+	DEVID    BIGINT,
+	VARID    BIGINT,
+	VALUE    BIGINT,
+	QUALITY  BOOLEAN)
+with (KAFKA_TOPIC='streaming-data', VALUE_FORMAT='JSON', KEY='devId', TIMESTAMP='date');
+
+create stream static_data (
+	BB_S_N         	 VARCHAR(STRING),           
+	MACHINE         VARCHAR(STRING) ,          
+	CDI             VARCHAR(STRING) ,          
+	ITEMID          VARCHAR(STRING) ,          
+	STARDTDATETIME  BIGINT,
+	ENDDATETIME     BIGINT,
+	VARID           BIGINT,
+	VALUE           BIGINT,
+	QUALITY         BOOLEAN )
+with (KAFKA_TOPIC='static-data', VALUE_FORMAT='JSON', KEY='bb_s_n', TIMESTAMP='enddatetime');
+
